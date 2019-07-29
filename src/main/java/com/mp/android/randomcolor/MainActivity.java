1f8f36bc4e5ed10823f8af66838ef721cfc7df8f
martaprosniak <martaprosniak@gmail.com>
2019-07-29 12:39:20 +0200

    basic app functionality applied

--- src/main/java/com/mp/android/randomcolor/MainActivity.java
+++ src/main/java/com/mp/android/randomcolor/MainActivity.java
@@ -4,21 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private View appView;
     private Button tryMeButton;
+    private int[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //create colors array
+        colors = new int[]{Color.CYAN, Color.GRAY, Color.GREEN, Color.RED, Color.BLACK, Color.BLUE,
+                Color.GREEN, Color.MAGENTA, Color.YELLOW};
+
         appView = findViewById(R.id.windowViewId);
-        appView.setBackgroundColor(Color.RED);
-        tryMeButton = (Button) findViewById(R.id.tryMeButton);
+
+        tryMeButton = findViewById(R.id.tryMeButton);
+        tryMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int colorArrayLength = colors.length;
+                Random random = new Random();
+                int randomNum = random.nextInt(colorArrayLength);
+                appView.setBackgroundColor(colors[randomNum]);
+
+                Log.d("Random", String.valueOf((randomNum)));
+            }
+        });
+
     }
 }
